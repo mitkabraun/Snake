5fd88c1df36ab93fe73621e33b9d3416473b2092
mitkabraun <mitkabraun@gmail.com>
2024-10-31 21:01:22 +0300

    Clean

--- Snake/App.xaml.cs
+++ Snake/App.xaml.cs
@@ -1,14 +1,3 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿namespace Snake;
 
-namespace Snake
-{
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
-
-}
+public partial class App;
